1466ccf63133a0fc5e88444f5250cb6447243f4c
ricedrink <alicezhang32092@gmail.com>
2020-09-14 13:25:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RESTfulAPI
+RESTful API app for sending requests and receiving responses
---
+++ RestManager/API.swift
@@ -0,0 +1,30 @@
+//
+//  API.swift
+//  RestManager
+//
+//  Created by Alice Zhang on 7/25/20.
+//  Copyright © 2020 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class API: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
